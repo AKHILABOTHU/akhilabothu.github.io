92630965657934c2f745f2769cd410a6d3cf11fc
AKHIL ABOTHU <abothuakhil@gmail.com>
2020-10-03 17:07:34 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+new Vue({
+	el:'#app',
+	data:{
+		user:{
+			name:'Akhil Abothu',
+			age:'23',
+			city:'Hyderabad',
+			email:'abothuakhil@gmail.com',
+			phone:'(+91) 7731801667',
+			role:'Associate Software Developer',
+			bio:' To get a job in a place where I can work by using all my knowledge and abilities in a productive,efficient, and creative manner to explore and upgrade myself with periodically changing technology.'
+		}
+	}
+	
+});
